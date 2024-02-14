7c738dcbdf724379995a3a0a8680f0638dd74342
shanmugappriyaa <pspri03@gmail.com>
2024-02-14 12:33:03 +0530

    Cors error

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ app.all('/*', function(req, res, next) {
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://shan-notes-mgmt.netlify.app/", //"http://localhost:5173",
     credentials: true,
+    origin: "*", //"http://localhost:5173",
+    methods: ["GET", "POST"],
     optionSuccessStatus: 200,
   })
 );
