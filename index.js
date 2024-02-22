debac0fa4bb2d9788fc9d06f2da036d0ff881723
shanmugappriyaa <pspri03@gmail.com>
2024-02-22 17:08:25 +0530

    cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Port = process.env.PORT;
 const app = express();
 
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header(
       "Access-Control-Allow-Methods",
